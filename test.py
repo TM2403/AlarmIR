3634aba92a203cf6ba5b391987b97314fae3f889
tmiyake1824 <tsugunobu.miyake@hotmail.co.jp>
2021-06-14 22:37:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -8,13 +8,32 @@ INPUT_GPIO = 17
 system = irrp_lib("../IRcodes")
 system.prepare(OUTPUT_GPIO, "P", "light", False)
 
-minutes = datetime.now().minute + 1
+hour_input = -1
+while hour_input < 0 or hour_input > 24:
+    hour_input = int(input("Hour: "))
 
-#while True:
+minute_input = -1
+while minute_input < 0 or minute_input > 60:
+    minute_input = int(input("Minute: "))
+
+
 t = datetime.now() # + timedelta(days=1)
-t = t.replace(hour=22, minute=31, second=0)
+t = t.replace(hour=hour_input, minute=minute_input, second=0)
 
 while datetime.now() < t:
     time.sleep(1)
 
 system.run()
+
+"""
+while True:
+    t = datetime.now() + timedelta(minutes=1)
+    t = t.replace(second=0)
+
+    while datetime.now() < t:
+        time.sleep(1)
+
+    system.run()
+    time.sleep(1)
+    system.run()
+"""
