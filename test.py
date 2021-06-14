5b37aa3eec89ebc03ed905e3a26c8f3d5b2c47fc
tmiyake1824 <tsugunobu.miyake@hotmail.co.jp>
2021-06-14 20:23:13 +0900

    Another Test

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 from irrp_lib import irrp_lib
+import time
+import sched
 
+scheduler = sched.scheduler(time.time, time.sleep)
 system = irrp_lib("../IRcodes")
-system.run(18, "P", "light", False)
+
+
+scheduler.enter(2, 1, system.run(18, "P", "light", False), ('first',))
+scheduler.enter(4, 1, system.run(18, "P", "light", False), ('second',))
+
+
+
